5fc4333c4b32360a7f69f38ca65457b423a464e7
Sidney Richards <sidney@maykinmedia.nl>
2025-05-28 09:54:34 +0200

    Allow exceptions in Sites config step to bubble up and be reported

--- tests/sites/test_sites_configuration_step.py
+++ tests/sites/test_sites_configuration_step.py
@@ -1,11 +1,23 @@
+from io import StringIO
+
 from django.contrib.sites.models import Site
-from django.core.exceptions import ImproperlyConfigured
+from django.core.exceptions import ImproperlyConfigured, ValidationError
+from django.core.management import CommandError, call_command
 
 import pytest
 
 from django_setup_configuration.contrib.sites.steps import SitesConfigurationStep
-from django_setup_configuration.exceptions import ConfigurationRunFailed
 from django_setup_configuration.test_utils import execute_single_step
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture(autouse=True)
+def include_test_step(settings):
+    settings.SETUP_CONFIGURATION_STEPS = settings.SETUP_CONFIGURATION_STEPS + [
+        SitesConfigurationStep
+    ]
+
 
 CONFIG_FILE_PATH = "tests/sites/files/sites.yaml"
 CONFIG_FILE_PATH_INVALID = "tests/sites/files/sites_invalid.yaml"
@@ -76,7 +88,7 @@ def test_execute_configuration_step_failing_should_keep_existing_sites():
         domain="example.com", defaults={"name": "example"}
     )
 
-    with pytest.raises(ConfigurationRunFailed):
+    with pytest.raises(ValidationError):
         execute_single_step(
             SitesConfigurationStep, yaml_source=CONFIG_FILE_PATH_INVALID
         )
@@ -124,3 +136,25 @@ def test_execute_configuration_step_creates_a_current_site_if_no_sites_or_site_i
     # (which would allow Django to match on the host).
     with pytest.raises(ImproperlyConfigured):
         Site.objects.get_current()
+
+
+def test_command_reports_validation_error():
+    stdout, stderr = StringIO(), StringIO()
+
+    with pytest.raises(CommandError) as excinfo:
+        call_command(
+            "setup_configuration",
+            yaml_file=CONFIG_FILE_PATH_INVALID,
+            stdout=stdout,
+            stderr=stderr,
+        )
+
+    assert (
+        "Error while executing step `Sites configuration`\n    {'domain': ['The domain"
+        " name cannot contain any spaces or tabs.']}\n" in stderr.getvalue()
+    )
+    assert (
+        str(excinfo.value)
+        == "Aborting run due to a failed step. All database changes have been rolled "
+        "back."
+    )
